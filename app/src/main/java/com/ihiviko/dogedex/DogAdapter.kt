11ddc364044a1695fdb013f3675687c4c964ac62
ivette <iconstanzav1@gmail.com>
2024-02-12 20:42:42 -0300

    se agrego la clase adapter

---
+++ app/src/main/java/com/ihiviko/dogedex/DogAdapter.kt
@@ -0,0 +1,44 @@
+package com.ihiviko.dogedex
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ihiviko.dogedex.databinding.DogListItemBinding
+
+
+class DogAdapter : ListAdapter<Dog, DogAdapter.DogViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Dog>(){
+
+        //comparar elementos en la lista y determinar si han cambiado-----------
+        override fun areItemsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+                return oldItem.id == newItem.id
+            }
+        //-----------------------------------------------------------------------
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val binding = DogListItemBinding.inflate(LayoutInflater.from(parent.context)) //inflamos la vista
+       return DogViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(dogViewHolder: DogViewHolder, position: Int) { //pintando el viewholder
+        val dog = getItem(position)
+        dogViewHolder.bind(dog)
+    }
+
+    //crear viewholder
+    inner class DogViewHolder(private val binding: DogListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        //pintamos la vista
+        fun bind(dog:Dog){
+            binding.dogName.text = dog.name //texto seteado xml no esta texto asociado
+        }
+    }
+}
