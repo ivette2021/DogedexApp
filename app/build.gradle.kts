7270ce4d43a567a40148fa934f5f53aaf6154914
ivette <iconstanzav1@gmail.com>
2024-02-13 19:33:50 -0300

    se implemento Viewmodel , por el momento el repositorio esta funcionando con datos locales implementaron corritunas , livedata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/ihiviko/dogedex/DogListViewModel.kt
+++ app/src/main/java/com/ihiviko/dogedex/DogListViewModel.kt
@@ -1,19 +1,29 @@
 package com.ihiviko.dogedex
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class DogListViewModel: ViewModel() {
+class DogListViewModel : ViewModel() {
+
+
+    private val _dogList = MutableLiveData<List<Dog>>()
+
+    //principio de encapsulamiento  livedata solo se podra leer pero no editar(fuera del viewmodel)
+    val dogList: LiveData<List<Dog>>
+        get() = _dogList
 
     private val dogRepository = DogRepository()
+
     init {
         downloadDogs()
     }
 
     private fun downloadDogs() {
         viewModelScope.launch {
-
+            _dogList.value = dogRepository.downloadDogs()
         }
     }
 }
