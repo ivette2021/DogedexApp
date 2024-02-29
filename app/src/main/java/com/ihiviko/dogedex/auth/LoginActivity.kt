dfa36bfd0d0898befcaecd2331ea3bc1545358ce
ivette <iconstanzav1@gmail.com>
2024-02-28 23:23:12 -0300

    creando pantallas de autentificacion

---
+++ app/src/main/java/com/ihiviko/dogedex/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ihiviko.dogedex.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ihiviko.dogedex.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
